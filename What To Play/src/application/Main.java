9fa04c7d5b05fb955516e99056f49c8ba6c45933
Ryan Bulcher <62976358+rbulcher@users.noreply.github.com>
2020-10-14 12:26:14 -0400

    Add files via upload

--- What To Play/src/application/Main.java
+++ What To Play/src/application/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -26,5 +27,6 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+		
 	}
 }
